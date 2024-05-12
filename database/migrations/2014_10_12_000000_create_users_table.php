7c481213c99047456a74f4a1c76616b9bc8751c7
Aastha <pdlpangali111@gmail.com>
2024-05-12 11:59:10 +0545

    controlelr , model and migration needed done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('Phone');
+            $table->boolean('Isadmin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
